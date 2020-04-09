cc6b90e19da03ada5a2da9db2ecc41b3cd326f60
verewygt <verewygt@users.noreply.github.com>
2020-04-10 00:38:23 +0200

    0.1.4.6 Dev

--- js/ga-optout.js
+++ js/ga-optout.js
@@ -1,37 +1,41 @@
 var gaProperty = 'UA-162970659-1';
 var disableStr = 'ga-disable-' + gaProperty;
 
-document.onload = function disableGA() {
-  if (document.cookie.indexOf(disableStr + '=true') > -1) {
+if (document.cookie.indexOf(disableStr + '=true') > -1) {
     window[disableStr] = true;
-  }
 }
 
-window.onload = function cookieHandler() {
+function cookieHandler() {
+    console.log("debug 1");
     if (document.cookie.indexOf(disableStr + '=true') > -1) {
+        console.log("debug 2");
         var banner = document.getElementById('cookie-banner');
         banner.style = "display: none;";
 
         if (window.location.href == "https://verewygt.github.io/privacy/") {
+            console.log("debug 3");
             var optout = document.getElementById('optout-msg');
             optout.textContent = " Opt-out cookie was set in your browser.";
         }
 
     } else if (document.cookie.indexOf('allowCookies=true') > -1) {
+        console.log("debug 4");
         var banner = document.getElementById('cookie-banner');
         banner.style = "display: none;";
     }
 }
 
+document.window.onload = cookieHandler();
+
 function gaOptout() {
-  document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+  document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/;';
   window[disableStr] = true;
   var optout = document.getElementById('optout-msg');
   optout.textContent = " Opt-out cookie was set in your browser.";
 }
 
 function acceptCookies() {
-  document.cookie = 'allowCookies=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+  document.cookie = 'allowCookies=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/;';
   var banner = document.getElementById('cookie-banner');
   banner.style = "display: none;";
 }
