1c68c0a7c95451c5fa582b3ff1b2d0f3571323a2
verewygt <verewygt@users.noreply.github.com>
2020-04-09 18:30:08 +0200

    0.1.3 Dev

--- js/ga-optout.js
+++ js/ga-optout.js
@@ -3,7 +3,7 @@ var gaProperty = 'UA-162970659-1';
 var disableStr = 'ga-disable-' + gaProperty;
 if (document.cookie.indexOf(disableStr + '=true') > -1) {
   window[disableStr] = true;
-  if (window.location.href == "https://verewygt.github.io/privacy") {
+  if (window.location.href == "https://verewygt.github.io/privacy/") {
       var optout = document.getElementById('optout-msg');
       optout.textContent = "Opt-out cookie was set in your browser.";
   }
