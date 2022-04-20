4d703b6c4661fe8129ce16690370d8967884c5aa
verewygt <62360150+verewygt@users.noreply.github.com>
2022-04-20 15:20:55 +0200

    Release 1.0.8.2
    - Google Analytics Update

--- perkroulette/js/ga-optout.js
+++ perkroulette/js/ga-optout.js
@@ -1,47 +1,35 @@
-var gaProperty = 'UA-162970659-1';
-var disableStr = 'ga-disable-' + gaProperty;
-
-if (document.cookie.indexOf(disableStr + '=true') > -1) {
-    window[disableStr] = true;
-}
-if (document.cookie.indexOf('allowCookies=true') > -1) {
-    ga('create', 'UA-162970659-1', 'auto');
-    ga('set', 'anonymizeIp', true);
-    ga('send', 'pageview');
-}
 
 window.addEventListener("load", function() {
-    if (document.cookie.indexOf(disableStr + '=true') > -1) {
-        var banner = document.getElementById('cookie-banner');
+
+    var banner = document.getElementById('cookie-banner');
+
+    if (document.cookie.indexOf("showCookieBanner=false") > -1) {
         banner.style = "display: none;";
-
-        if (window.location.href == "https://verewygt.github.io/privacy/") {
-            var optout = document.getElementById('optout-msg');
-            optout.textContent = " Opt-out cookie was set in your browser.";
-        }
-
-    } else if (document.cookie.indexOf('allowCookies=true') < 0) {
-        var banner = document.getElementById('cookie-banner');
+    } else {
         banner.style = "display: flex;";
-    } else if (document.cookie.indexOf('allowCookies=true') > -1) {
-        var banner = document.getElementById('cookie-banner');
-        banner.style = "display: none;";
     }
+
 });
 
-function gaOptout() {
-  document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/;';
-  window[disableStr] = true;
-  var optout = document.getElementById('optout-msg');
-  optout.textContent = " Opt-out cookie was set in your browser.";
+function denyCookies() {
+    document.cookie = "showCookieBanner=false; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/";
+
+    var banner = document.getElementById('cookie-banner');
+    banner.style = "display: none;";
+
+    if (window.location.href.indexOf("privacy") > -1) {
+        var optoutbutton = document.getElementById('optOutButton');
+        optoutbutton.innerHTML = "OPT-OUT SET";
+    }
+
+    gtag('consent', 'update', {'analytics_storage': 'denied'});
 }
 
 function acceptCookies() {
-  document.cookie = 'allowCookies=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/;';
-  var banner = document.getElementById('cookie-banner');
-  banner.style = "display: none;";
+    document.cookie = "showCookieBanner=false; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/";
 
-  ga('create', 'UA-162970659-1', 'auto');
-  ga('set', 'anonymizeIp', true);
-  ga('send', 'pageview');
+    var banner = document.getElementById('cookie-banner');
+    banner.style = "display: none;";
+
+    gtag('consent', 'update', {'analytics_storage': 'granted'});
 }
